56bc861521a0d68eca110c771ab3fb2d27ca348c
Mark Jeo Torrena <marktorrena1@gmail.com>
2018-04-28 23:05:00 +0800

    added form in log in using reactive form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -6,11 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  formLogin: FormGroup;
+  private formSubmitAttempt: boolean;
   title:string = "HELLLO";
-  constructor() { }
+
+  constructor( private fb: FormBuilder) { }
 
   ngOnInit() {
-   this.title = "YA YA YA ";
+   this.formLogin = this.fb.group({
+    userName: ['',Validators.required],
+    password: ['',Validators.required]
+   });
+  }
+
+  isFieldInvalid(field: string) { // {6}
+    return (
+      (!this.formLogin.get(field).valid && this.formLogin.get(field).touched) ||
+      (this.formLogin.get(field).untouched && this.formSubmitAttempt)
+    );
+  }
+
+  onSubmit(){
+    if(this.formLogin.valid) {
+      //call service to try login;
+      console.log(this.formLogin.value);
+    }
+    this.formSubmitAttempt = true;
   }
 
 }
